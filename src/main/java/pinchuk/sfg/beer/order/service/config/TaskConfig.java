8362f6b8d6e7c748991e01eebf3fd4990a133b94
Pinchuk Yevhen <jagernaut1233@gmail.com>
2020-03-18 10:38:51 +0200

    Initial commit

---
+++ src/main/java/pinchuk/sfg/beer/order/service/config/TaskConfig.java
@@ -0,0 +1,22 @@
+package pinchuk.sfg.beer.order.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author Pinchuk Yevhen
+ * @created 18/03/2020 - 10:34
+ */
+@EnableScheduling
+@EnableAsync
+@Configuration
+public class TaskConfig {
+    @Bean
+    TaskExecutor taskExecutor() {
+        return new SimpleAsyncTaskExecutor();
+    }
+}
