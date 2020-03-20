a82b411760a92f735586cd87a2fe06625ec74089
Pinchuk Yevhen <jagernaut1233@gmail.com>
2020-03-20 21:07:24 +0200

    added jms

---
+++ src/main/java/pinchuk/sfg/beer/order/service/config/JmsConfig.java
@@ -0,0 +1,23 @@
+package pinchuk.sfg.beer.order.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * @author Pinchuk Yevhen
+ * @created 20/03/2020 - 21:02
+ */
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
