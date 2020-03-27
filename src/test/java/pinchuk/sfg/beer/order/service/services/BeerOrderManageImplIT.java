16b154b9027597ca4816c45873e8a1c5640348bf
Pinchuk Yevhen <jagernaut1233@gmail.com>
2020-03-27 21:08:34 +0200

    added WireMock

--- src/test/java/pinchuk/sfg/beer/order/service/services/BeerOrderManageImplIT.java
+++ src/test/java/pinchuk/sfg/beer/order/service/services/BeerOrderManageImplIT.java
@@ -1,19 +1,32 @@
 package pinchuk.sfg.beer.order.service.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.jenspiegsa.wiremockextension.WireMockExtension;
+import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import pinchuk.sfg.beer.order.service.domain.BeerOrder;
 import pinchuk.sfg.beer.order.service.domain.BeerOrderLine;
 import pinchuk.sfg.beer.order.service.domain.Customer;
 import pinchuk.sfg.beer.order.service.repositories.BeerOrderRepository;
 import pinchuk.sfg.beer.order.service.repositories.CustomerRepository;
+import pinchuk.sfg.beer.order.service.services.beer.BeerServiceImpl;
+import pinchuk.sfg.brewery.model.BeerDto;
 
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
+import static com.github.jenspiegsa.wiremockextension.ManagedWireMockServer.with;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static pinchuk.sfg.beer.order.service.domain.BeerOrderStatusEnum.ALLOCATED;
@@ -22,6 +35,7 @@ import static pinchuk.sfg.beer.order.service.domain.BeerOrderStatusEnum.ALLOCATE
  * @author Pinchuk Yevhen
  * @created 26/03/2020 - 21:48
  */
+@ExtendWith(WireMockExtension.class)
 @SpringBootTest
 public class BeerOrderManageImplIT {
 
@@ -34,9 +48,26 @@ public class BeerOrderManageImplIT {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    WireMockServer wireMockServer;
+
     Customer testCustomer;
 
     UUID beerId = UUID.randomUUID();
+
+    @TestConfiguration
+    static class RestTemplateBuilderProvider {
+
+        @Bean(destroyMethod = "stop")
+        public WireMockServer wireMockServer() {
+            WireMockServer server = with(wireMockConfig().port(8083));
+            server.start();
+            return server;
+        }
+    }
 
     @BeforeEach
     void setUp() {
@@ -46,7 +77,13 @@ public class BeerOrderManageImplIT {
     }
 
     @Test
-    void testNewToAllocated() {
+    void testNewToAllocated() throws JsonProcessingException {
+
+        BeerDto beerDto = BeerDto.builder().id(beerId).upc("12345").build();
+//        BeerPagedList list = new BeerPagedList(List.of(beerDto));
+
+        wireMockServer.stubFor(get(BeerServiceImpl.BEER_UPC_PATH_V1 + "12345")
+            .willReturn(okJson(objectMapper.writeValueAsString(beerDto))));
         BeerOrder beerOrder = createBeerOrder();
         BeerOrder savedBeer = beerOrderManager.newBeerOrder(beerOrder);
 
@@ -54,13 +91,14 @@ public class BeerOrderManageImplIT {
         assertEquals(ALLOCATED, savedBeer.getOrderStatus());
     }
 
-    public BeerOrder createBeerOrder(){
+    public BeerOrder createBeerOrder() {
         BeerOrder beerOrder = BeerOrder.builder()
                 .customer(testCustomer)
                 .build();
         Set<BeerOrderLine> lines = new HashSet<>();
         lines.add(BeerOrderLine.builder()
                 .beerId(beerId)
+                .upc("12345")
                 .orderQuantity(1)
                 .beerOrder(beerOrder)
                 .build());
